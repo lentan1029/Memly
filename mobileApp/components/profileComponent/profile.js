3e03d32b4cd452beb036e570c32f31b7c040c051
Alex Pantelides <alex.pantelides@gmail.com>
2016-09-16 13:43:19 -0700

    Fix profile page. Need data!

---
+++ mobileApp/components/profileComponent/profile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Button from 'react-native-button';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Actions} from 'react-native-router-flux';
+
+var Profile = (props) => (
+
+  <View>
+    <View style={styles.imageContainer}>
+      <Image source={{uri: props.picture}} style={styles.image} />
+    </View>
+
+    <View style={styles.infoContainer}>
+      <Text style={styles.info}>Name: {props.firstName + ' ' + props.lastName}</Text>
+      <Text style={styles.info}>Email: {props.email}</Text>
+      <Text style={styles.info}>Gender: {props.gender}</Text>
+      <Text style={styles.info}>Birthday: {props.birthday}</Text>
+    </View>
+
+    <View style={styles.buttonLocation}>
+      <Button onPress={() => {
+        props.hideSideMenu();
+        Actions.EditProfilePage();
+      }}
+        containerStyle={styles.buttonContainer}
+        style={styles.button}>
+        Edit Profile
+      </Button>
+    </View>
+
+  </View>
+);
+
+module.exports = Profile;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    padding: 10,
+    height: 45,
+    width: 150,
+    overflow: 'hidden',
+    borderRadius: 4,
+    backgroundColor:
+    '#0288D1'
+  },
+  button: {
+    fontSize: 20,
+    color: 'white'
+  },
+  buttonLocation: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 45
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: 'lightblue',
+    borderWidth: 5,
+  },
+
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 75
+  },
+  infoContainer: {
+    marginTop: 45,
+    marginLeft: 35
+  },
+  info: {
+    marginTop: 20
+  }
+});
